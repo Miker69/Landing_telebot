77a5ac819ed089a9ed690d7ff243fe49c26d4c6e
root <root@localhost.localdomain>
2021-04-13 16:22:31 +0200

    final version

---
+++ Landing/urls.py
@@ -0,0 +1,32 @@
+"""Landing URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from crm import views
+from django.conf.urls.static import static
+from django.conf import settings
+from .settings import DEBUG
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.first_page),
+    path('thanks/', views.thanks_page, name='thanks_page')
+]
+if DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  #  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#https://api.telegram.org/bot1799417757:AAEBnKrRBi7A7d9A6939JIwMY2Ga3aaWXPw/sendMessage?chat_id=-524341417&text=прием!
